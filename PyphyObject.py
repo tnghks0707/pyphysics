2767f60d6ba28632efc6926de4980f505883c7e4
tnghks0707 <tnghks481@gmail.com>
2021-09-20 00:35:31 +0900

    make engine into object

--- PyphyObject.py
+++ PyphyObject.py
@@ -1,17 +1,73 @@
 import pygame
+import ctypes
 
 class Object:
     Surface: pygame.Surface
     Rect: pygame.Rect
+    rigidbody = None
 
-    def __init__(self, Isurface):
+    def __init__(self, Isurface, rect=None):
         self.Surface = Isurface
         self.Rect = self.Surface.get_rect()
+        if rect != None:
+            self.Rect = rect
+    
+    def SetRigidbody(self, mass):
+        self.rigidbody = Rigidbody(self.Rect, mass)
+
+    def Render(self, displaysurf):
+        displaysurf.blit(self.Surface, self.Rect)
+
+
 
 class Rigidbody:
+    Rect = None
     Mass = 0.0
     HeightF = 0.0
-    HeightV = 0.0
     HeightA = 0.0
-    def __init(self, ):
+    HeightDeltaA = 0.0
+    HeightDistence = 0.0
+    HeightF_lasttime = 0
+
+    def __init__(self, Rect, mass):
+        self.Rect = Rect
+        self.Mess = mass
         pass
+
+    def AddForce(self, vector):
+        self.HeightF += vector.getHeightF()
+
+    def Calculate(self, timeDelta):
+        self.HeightA = self.HeightF / self.Mess
+        self.HeightF = 0
+        
+        (imageWidth, imageHeight) = self.Rect.topleft
+
+        timedelta_f = timeDelta / 1000
+        #지속시간에 더함
+        #timecontinue += timedelta_f
+
+        self.HeightDeltaA += self.HeightA * timedelta_f
+        self.HeightDistence -= self.HeightDeltaA * timedelta_f
+    
+        distence_pixel = self.HeightDistence * 100
+
+        self.Rect.topleft = (imageWidth, distence_pixel)
+
+        print(self.HeightDeltaA)
+
+        if(distence_pixel > 1500):
+            self.HeightDistence = -10.0
+
+
+
+class Vector:
+    HeightF = 0.0
+    WidthF = 0.0
+    
+    def __init__(self, HeightF, WidthF):
+        self.HeightF = HeightF
+        self.WidthF = WidthF
+
+    def getHeightF(self):
+        return self.HeightF
--- main.py
+++ main.py
@@ -17,52 +17,27 @@ clock = pygame.time.Clock()
 
 #pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 
-myimage = pygame.image.load("cur.png")
-imagerect : pygame.Rect = myimage.get_rect()
+po = PyphyObject.Object(pygame.image.load("cur.png"))
+po.SetRigidbody(1.0)
 
-po = PyphyObject.Object(myimage)
 imageWidth = 0
 imageHeight = 0
 timedelta = 0
 #imagerect.center = (width_, height_)
 #imagerect.center = (0, 0)
 
-#물리엔진
-F = -9.8
-m = 1.0
-a = F / m
-deltaA = 0.0
-distence = 0.0
-timecontinue = 0.0
-
 while True:
+    timedelta = clock.tick(fps)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    
-    #물리엔진 시작
-    
-    #시간 변화량을 초 단위로 환산
-    timedelta_f = timedelta / 1000
-    #지속시간에 더함
-    timecontinue += timedelta_f
 
-    deltaA += a * timedelta_f
-    distence -= deltaA * timedelta_f
-    
-    distence_pixel = distence * 100
-
-    imageHeight = distence_pixel
-
-    if(distence_pixel > 1500):
-        distence = -10.0
-
-    #물리엔진 끝
-    #imageWidth, imageHeight = pygame.mouse.get_pos()
     displaysurf.fill(black)
-    imagerect.topleft = (imageWidth, imageHeight)
-    displaysurf.blit(myimage, imagerect)
+
+    po.rigidbody.AddForce(PyphyObject.Vector(-9.8, 0))
+    po.rigidbody.Calculate(timedelta)
+    po.Render(displaysurf)
 
     pygame.display.update()
-    timedelta = clock.tick(fps)
