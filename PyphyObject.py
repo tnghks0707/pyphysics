0b02ee2833078c0ec4989b5167cc418ab1fc3811
tnghks0707 <tnghks481@gmail.com>
2021-09-22 00:35:41 +0900

    make object from json(perfect) & remove distence_pixel

--- PyphyObject.py
+++ PyphyObject.py
@@ -5,20 +5,29 @@ class Object:
     Surface: pygame.Surface
     Rect: pygame.Rect
     rigidbody = None
+    Visible = True
 
-    def __init__(self, Isurface, rect=None, name=None):
+    def __init__(self, Isurface, rect=None, name=None, size=None, visible=True):
         self.Surface = Isurface
-        self.Rect = self.Surface.get_rect()
+        self.Visible = visible
         if rect != None:
             self.Rect = rect
+        else:
+            self.Rect = self.Surface.get_rect()
         if name != None:
             self.Name = name
+        if size != None:
+            self.Surface = pygame.transform.smoothscale(self.Surface, size)
     
     def SetRigidbody(self, mass):
         self.rigidbody = Rigidbody(self.Rect, mass)
 
+    def TransformScale(self, size):
+        self.Surface = pygame.transform.smoothscale(self.Surface, size)
+
     def Render(self, displaysurf):
-        displaysurf.blit(self.Surface, self.Rect)
+        if self.Visible == True:
+            displaysurf.blit(self.Surface, self.Rect)
 
 
 
@@ -28,12 +37,17 @@ class Rigidbody:
     HeightF = 0.0
     HeightA = 0.0
     HeightDeltaA = 0.0
-    HeightDistence = 0.0
-    HeightF_lasttime = 0
 
-    def __init__(self, Rect, mass):
+    HeightDistence = 0.0
+
+    HeightF_lasttime = 0
+    
+    WidthDistence = 0.0
+
+    def __init__(self, Rect: pygame.Rect, mass):
         self.Rect = Rect
         self.Mess = mass
+        (self.WidthDistence, self.HeightDistence) = Rect.topleft
         pass
 
     def AddForce(self, vector):
@@ -42,23 +56,19 @@ class Rigidbody:
     def Calculate(self, timeDelta):
         self.HeightA = self.HeightF / self.Mess
         self.HeightF = 0
-        
-        (imageWidth, imageHeight) = self.Rect.topleft
 
         timedelta_f = timeDelta / 1000
         #지속시간에 더함
         #timecontinue += timedelta_f
 
         self.HeightDeltaA += self.HeightA * timedelta_f
-        self.HeightDistence -= self.HeightDeltaA * timedelta_f
-    
-        distence_pixel = self.HeightDistence * 100
+        self.HeightDistence -= self.HeightDeltaA * timedelta_f * 100
 
-        self.Rect.topleft = (imageWidth, distence_pixel)
+        self.Rect.topleft = (self.WidthDistence, self.HeightDistence)
 
         print(self.HeightDeltaA)
 
-        if(distence_pixel > 1500):
+        if(self.HeightDistence > 1500):
             self.HeightDistence = -10.0
 
 
--- main.py
+++ main.py
@@ -4,16 +4,40 @@ import sys
 import json
 from pygame.locals import *
 
+ObjectList = []
+
 #Build Objects from Object.json
 def BuildObjects():
     jsonObject = json.load(open("Objects.json"))
-    print(jsonObject["PyphyVersion"])
-    pass
+    version = jsonObject["PyphyVersion"]
+    global ObjectList
+    for i in jsonObject["Objects"]:
+        name = i["Name"]
+        location = i["Location"]
+        size = i["Size"]
+        size_height = size[0]
+        size_width = size[1]
+        image = i["Image"]
+        visible = i["Visible"]
+
+        rigidbody = None
+        if "Rigidbody" in i:
+            rigidbody = i["Rigidbody"]
+
+        image = pygame.image.load(image)
+        rect : pygame.Rect = image.get_rect()
+        rect.topleft = (location[0], location[1])
+        newObject = PyphyObject.Object(image, rect=rect, name=name, size=(size_height, size_width), visible=visible)
+        if rigidbody != None:
+            newObject.SetRigidbody(rigidbody["Mass"])
+
+        newObject.rigidbody.AddForce(PyphyObject.Vector(0, -50))
+        
+        ObjectList.append(newObject)
 
 def main():
     BuildObjects()
-    return
-    
+
     width = 1280
     height = 720
     white = (255, 255, 255)
@@ -26,16 +50,29 @@ def main():
     displaysurf = pygame.display.set_mode((width, height), 0, 32)
     clock = pygame.time.Clock()
 
+    """
+    pic = pygame.image.load("cur.png")
+    pic2 = pygame.transform.scale(pic, (320, 240))
+    pic = pygame.transform.smoothscale(pic, (320, 240))
+    """
+
     #pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 
     #오브젝트 생성
+
+    """
     po = PyphyObject.Object(pygame.image.load("cur.png"))
     po.SetRigidbody(1.0)
     po.rigidbody.AddForce(PyphyObject.Vector(0, -9.8))
+    """
 
     timedelta = 0
     #imagerect.center = (width_, height_)
     #imagerect.center = (0, 0)
+
+    pygame.event.clear()
+
+    global ObjectList
 
     while True:
         timedelta = clock.tick(fps)
@@ -48,10 +85,17 @@ def main():
 
         displaysurf.fill(black)
 
+        for Object in ObjectList:
+            #Object.rigidbody.AddForce(PyphyObject.Vector(0, -9.8))
+            Object.rigidbody.Calculate(timedelta)
+            Object.Render(displaysurf)
+
         #중력가속도
-        #po.rigidbody.AddForce(PyphyObject.Vector(0, -9.8))
+        """
+        po.rigidbody.AddForce(PyphyObject.Vector(0, -9.8))
         po.rigidbody.Calculate(timedelta)
         po.Render(displaysurf)
+        """
 
         pygame.display.update()    
 
